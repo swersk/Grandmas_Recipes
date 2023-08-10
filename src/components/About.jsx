7a736682b44421685728ba94f189b80c56c59d0d
Lauren Swersky <lauren.swersky@gmail.com>
2023-08-10 18:06:29 -0400

    about text larger

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
         <img src="./us.jpg" alt="pic of Grandma and I" className="mt-20 h-[70vh] mb-36 ml-5" />
         <div className=" text-black p-4 text-start ml-12 mr-12 mt-16">
 
-        <span className="font-dancing-bold text-5xl">Welcome </span> <span className="font-satoshi-regular text-1xl">to this cherished, hidden corner of the internet! This page celebrates the cooking of our wonderful Grandma Jeanette.
+        <span className="font-dancing-bold text-5xl">Welcome </span> <span className="font-satoshi-regular text-xl">to this cherished, hidden corner of the internet! This page celebrates the cooking of our wonderful Grandma Jeanette.
           <br /><br />
           Grandma was blessed to be born in the best place on Earth: Bay Ridge, Brooklyn. Growing up she watched her mother Lucy cook in the kitchen. Infused with love, care, and Italian authenticity in everything she made, Grandma Lucy's cooking was a treat and a treasure.
           Her approach was grounded in a few core principles: Keep it simple. Do not waste food. Share with others.
